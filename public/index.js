257cb10b19a82dc6f607154c78f73fdd25a3b877
MIYA_NAKATA <MIYAN@UW.EDU>
2023-06-02 15:15:19 -0700

    added random comment

--- public/index.js
+++ public/index.js
@@ -197,6 +197,7 @@
     console.log(history['transaction-history']);
     if (history['transaction-history'].length === 0) {
       // user hasn't purchased anything
+      //
     } else {
       console.log('in else block of displayHistory()')
       // for (let i = 0; i < history['transaction-history'].length; i++) {
