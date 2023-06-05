df84fc7381f06b318c9a02b8c27b8f3ad7bdd96b
suzpiver <suzpiver@uw.edu>
2023-06-04 22:15:53 -0700

    created tables.sql file

---
+++ tables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "inventory" (
+	"itemID"	INTEGER NOT NULL,
+	"XS"	INTEGER NOT NULL,
+	"S"	INTEGER NOT NULL,
+	"M"	INTEGER NOT NULL,
+	"L"	INTEGER NOT NULL,
+	"XL"	INTEGER NOT NULL,
+	"XXL"	INTEGER NOT NULL,
+	PRIMARY KEY("itemID")
+)
+CREATE TABLE "items" (
+	"itemID"	NUMERIC NOT NULL,
+	"name"	TEXT NOT NULL,
+	"webname"	TEXT NOT NULL,
+	"type"	TEXT NOT NULL,
+	"color"	TEXT NOT NULL,
+	"price"	NUMERIC NOT NULL,
+	PRIMARY KEY("itemID")
+)
+CREATE TABLE "reviews" (
+	"rid"	INTEGER,
+	"itemID"	INTEGER,
+	"user"	TEXT,
+	"stars"	INTEGER,
+	"comments"	TEXT,
+	PRIMARY KEY("rid")
+)
+CREATE TABLE "transactions" (
+	"id"	INTEGER NOT NULL,
+	"confirmation"	TEXT NOT NULL,
+	"user"	TEXT NOT NULL,
+	"date"	INTEGER NOT NULL,
+	"itemID"	INTEGER NOT NULL,
+	"size"	INTEGER NOT NULL,
+	PRIMARY KEY("id")
+)
+CREATE TABLE "users" (
+	"username"	TEXT,
+	"password"	TEXT NOT NULL,
+	"email"	TEXT,
+	PRIMARY KEY("username")
+)
